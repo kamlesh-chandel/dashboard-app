f6670d2f65e1ee3c0cbdb31251cb8a93ef71468a
kamlesh-chandel <kamlesh@gkmit.co>
2026-02-06 15:57:12 +0530

    Resolved merge conflicts

--- src/app/Routes.jsx
+++ src/app/Routes.jsx
@@ -1,12 +1,20 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Login } from '@/features/auth/pages/login';
+import { lazy, Suspense } from 'react';
+
+const Dashboard = lazy(() => import("../features/dashboard/pages/dashboard"));
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
+        <Suspense fallback={<div>Loading .....</div>}>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={
+            <Dashboard />
+          } />
       </Routes>
+          </Suspense>
     </BrowserRouter>
   );
 };
