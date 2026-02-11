3a4e9a066d62447231b1bf51904a03fb22222829
kamlesh-chandel <kamlesh@gkmit.co>
2026-02-11 17:10:30 +0530

    fix: remove style prop from button

--- src/components/common/button/index.jsx
+++ src/components/common/button/index.jsx
@@ -1,9 +1,9 @@
 import './index.css';
 import '@/styles/theme.css';
 
-export const Button = ({ children,className="", varient="primary", style, ...props }) => {
+export const Button = ({ children,className="", varient="primary", ...props }) => {
   return (
-    <button className={`app-btn app-btn-${varient} ${className}`} style={style} {...props}>
+    <button className={`app-btn app-btn-${varient} ${className}`} {...props}>
       {children}
     </button>
   );
