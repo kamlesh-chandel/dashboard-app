f9be9c1520b6fa34c536b0383257195631164689
kamlesh-chandel <kamlesh@gkmit.co>
2026-02-13 01:52:09 +0530

    feat: resolve merge conflict

--- src/components/common/button/index.tsx
+++ src/components/common/button/index.tsx
@@ -1,11 +1,14 @@
-import type { ButtonProps } from '@/types/ui.types';
-
 import './index.css';
 import '@/styles/theme.css';
 
-export const Button: React.FC<ButtonProps> = ({ children,className="", variant="primary", ...props }) => {
+export const Button = ({
+  children,
+  className = '',
+  varient = 'primary',
+  ...props
+}) => {
   return (
-    <button className={`app-btn app-btn-${variant} ${className}`} {...props}>
+    <button className={`app-btn app-btn-${varient} ${className}`} {...props}>
       {children}
     </button>
   );
