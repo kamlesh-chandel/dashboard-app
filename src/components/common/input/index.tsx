79faff5dbbaa2ee77bf7158c7ba75232174163c9
kamlesh-chandel <kamlesh@gkmit.co>
2026-02-13 10:58:13 +0530

    refactor: resolve merge conflicts

--- src/components/common/input/index.tsx
+++ src/components/common/input/index.tsx
@@ -3,7 +3,13 @@ import type { InputProps } from '@/types/ui.types';
 import './index.css';
 import '@/styles/theme.css';
 
-export const Input: React.FC<InputProps> = ({ id, type = 'text', placeholder, value, onChange }) => {
+export const Input: React.FC<InputProps> = ({
+  id,
+  type = 'text',
+  placeholder,
+  value,
+  onChange,
+}) => {
   return (
     <input
       className="app-input"
