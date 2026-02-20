d0a2a1f39d0d5b784e378a193abb66d365be4c0b
kamlesh-chandel <kamlesh@gkmit.co>
2026-02-20 15:51:34 +0530

    refactor: add sanity credential in .env

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -1,10 +1,9 @@
 import { createClient } from '@sanity/client';
 
 export const sanityClient = createClient({
-  projectId: 'yui6o9rb',
-  dataset: 'production',
-  apiVersion: '2024-01-01',
-  token:
-    'skSzpeEe6L3suJsfOxv4LSVJJV4q4dumYJfxQRZdDEc1IEe7Vd2WHxK9279wKlMKgx1fqyv7MCb5jfLEhL70GvFyA7oFw7AdkPpj9EyrZGpFGb5ldKN1QG0oIoPl9hx8SQi5VqIrHOaPgxp84RLstOSVmbeZk0KQbELO66rqu4lt5ZcN18i5',
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+  dataset: import.meta.env.VITE_SANITY_DATASET,
+  apiVersion: import.meta.env.VITE_SANITY_API_VERSION,
+  token: import.meta.env.VITE_SANITY_TOKEN,
   useCdn: false,
 });
